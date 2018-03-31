b633106073a7c80c3791180d46d9b26681acb584
finbargiusti <finbargiusti@gmail.com>
2018-03-31 18:14:10 +0100

    Added some dumb shit incl. player movemen

--- src/client/script.js
+++ src/client/script.js
@@ -1,5 +1,7 @@
 const socket = new WebSocket("ws://" + window.location.host);
 
+let view = "x";
+
 Math.TAU = Math.PI * 2;
 
 let lastPlayerId = 0;
@@ -19,9 +21,15 @@ function render() {
     
     for (let id in players) {
         const player = players[id];
+
+        let canX, canY
+
+        switch (view) {
+            case "x": canX = player.pos.y; canY = player.pos.z;
+        }
         
-        let x = mainCanvas.width / 2 + player.pos.x,
-            y = mainCanvas.height / 2 + player.pos.y;
+        let x = mainCanvas.width / 2 + canX,
+            y = mainCanvas.height / 2 + canY;
         
         ctx.beginPath();
         ctx.arc(x, y, 5, 0, Math.TAU);
@@ -31,17 +39,46 @@ function render() {
     requestAnimationFrame(render);
 }
 
-let view = "x";
+function requestMove(direction) {
+    switch (view) {
+        case "x": {
+            if (direction == "left"){
+                players[0].pos.addVector(new Vector3D(0, -5, 0));
+            }
+            if (direction == "right"){
+                players[0].pos.addVector(new Vector3D(0, 5, 0));
+            }
+        };
+        case "y": {
+            if (direction == "left"){
+                players[0].pos.addVector(new Vector3D(0, -5, 0));
+            }
+            if (direction == "right"){
+                players[0].pos.addVector(new Vector3D(0, 5, 0));
+            }
+        };
+        case "z": {
+            if (direction == "left"){
+                players[0].pos.addVector(new Vector3D(0, -5, 0));
+            }
+            if (direction == "right"){
+                players[0].pos.addVector(new Vector3D(0, 5, 0));
+            }
+        };
+    }
+} 
 
 window.addEventListener("keydown", (e) => {
     console.log(e.keyCode) // A: 65, D: 68
     
     switch (e.keyCode)  {
         case 65: {
-            
-        }; break;
+            requestMove("left")
+            break;
+        };
         case 68: {
-            
-        }; break;
+            requestMove("right")
+            break;
+        };
     }
-});
+})  ;
