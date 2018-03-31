399f19a4cea865531ac96e573ba965d09c85b1fd
David Paryr <dpayr@musica-viva.de>
2018-03-31 21:58:02 +0200

    Made changes to binary.js structure

--- src/client/data_structures.js
+++ src/client/data_structures.js
@@ -16,16 +16,14 @@ var serverCommands = new binary.Array([new binary.Dynamic({
             vertical: new binary.Number("sByte")
         })
     }),
-    removePlayer: new binary.Number("uShort")
+    removePlayer: new binary.Number("uShort"),
+    changePerspective: new binary.String(1)
 })], "short");
 
 var clientCommands = new binary.Array([new binary.Dynamic({
     updateControls: new binary.Object({
         horizontal: new binary.Number("sByte"),
         vertical: new binary.Number("sByte")
-    })    
+    }),
+    togglePerspective: null
 })], "short");
-
-var sharedValues = {
-    playerMovementVelocity: 200
-};
