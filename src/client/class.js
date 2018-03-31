600e675433acc1c8a7d89236c12e3927a22b05d1
David Paryr <dpayr@musica-viva.de>
2018-03-31 18:18:52 +0200

    Added functionality to classes

--- src/client/class.js
+++ src/client/class.js
@@ -1,6 +1,8 @@
 class Player {
-    constructor() {
+    constructor(id) {
+        this.id = id;
         this.pos = new Vector3D(0, 0, 0);
+        this.vel = new Vector3D(0, 0, 0);
     }
 }
 
@@ -9,5 +11,21 @@ class Vector3D {
         this.x = x;
         this.y = y;
         this.z = z;
+    }
+    
+    addVector(v2) {
+        this.x += v2.x;
+        this.y += v2.y;
+        this.z += v2.z;
+    }
+    
+    scale(scalar) {
+        this.x *= scalar;
+        this.y *= scalar;
+        this.z *= scalar;
+    }
+    
+    getLength() {
+        return Math.sqrt(this.x*this.x + this.y*this.y + this.z*this.z);
     }
 }
