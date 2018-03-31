64586b568ce64ddf2170a26cb6d938f5c7d5afd1
David Paryr <dpayr@musica-viva.de>
2018-03-31 17:08:41 +0200

    Added basic web server

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,23 @@
 const http = require('http');
 const port = 5000;
+const url = require("url");
+const fs = require("fs");
 
 const server = http.createServer();
 
 server.on("request", function(request, response){
-	response.end();
+    const parsedURL = url.parse(request.url);
+    let path = parsedURL.path;
+    
+    if (path === "/") path += "index.html";
+    
+    fs.readFile(__dirname + "/../client" + path, (err, data) => {
+        if (!err) {
+            response.end(data.toString());
+        } else {
+            response.end("404");
+        }
+    });
 });
 
 server.listen(port);
