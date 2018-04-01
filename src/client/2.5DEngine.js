4c2a1a6a5ffe907cd28fd6bbbd9cbd73cae7c41b
David Paryr <dpayr@musica-viva.de>
2018-04-01 20:21:46 +0200

    Tried fixing face sorting bug

--- src/client/2.5DEngine.js
+++ src/client/2.5DEngine.js
@@ -3,6 +3,7 @@
         constructor() {
             this.objects = [];
             this.camera = new Camera();
+            this.showAxes = false;
         }
 
         project(v) {
@@ -13,7 +14,7 @@
         }
 
         render(ctx) {
-            const cameraVector = new Vector3D(Math.cos(this.camera.rotZ) * Math.cos(this.camera.rotUp), Math.sin(this.camera.rotZ) * Math.cos(this.camera.rotUp), Math.sin(this.camera.rotUp));
+            const cameraVector = new Vector3D(-Math.cos(this.camera.rotZ) * Math.cos(this.camera.rotUp), Math.sin(this.camera.rotZ) * Math.cos(this.camera.rotUp), Math.sin(this.camera.rotUp));
             const processedObjects = [];
 
             for (let i = 0; i < this.objects.length; i++) {
@@ -24,65 +25,91 @@
                     for (let j = 0; j < faces.length; j++) {
                         processedObjects.push(new ProcessedFace(faces[j], cameraVector));
                     }
+                } else if (object.constructor.name === "Point") {
+                    processedObjects.push(new ProcessedPoint(object, cameraVector));
                 }
             }
 
             processedObjects.sort(function(a, b) {
-                /*if (a.object.constructor.name === "Point" && b.object.constructor.name === "Point") {
-                    return a.dotProducts[0] - b.dotProducts[0];
-                }*/
+                
+                if (a.constructor.name === "ProcessedPoint" && b.constructor.name === "ProcessedPoint") {
+                    return a.dotProduct - b.dotProduct;
+                } else if (a.constructor.name === "ProcessedFace" && b.constructor.name === "ProcessedFace") {
+                    let aCount = 0,
+                        bCount = 0;
+
+                    if (a.dotProducts[0] > b.dotProducts[0]) aCount++; else if (a.dotProducts[0] < b.dotProducts[0]) bCount++;
+                    if (a.dotProducts[1] > b.dotProducts[1]) aCount++; else if (a.dotProducts[1] < b.dotProducts[1]) bCount++;
+                    if (a.dotProducts[2] > b.dotProducts[2]) aCount++; else if (a.dotProducts[2] < b.dotProducts[2]) bCount++;
+
+                    let sum1 = a.dotProducts[0] + a.dotProducts[1] + a.dotProducts[2],
+                        sum2 = b.dotProducts[0] + b.dotProducts[1] + b.dotProducts[2];
+
+                    let min1 = Math.min(Math.min(a.dotProducts[0], a.dotProducts[1]), a.dotProducts[2]),
+                        min2 = Math.min(Math.min(b.dotProducts[0], b.dotProducts[1]), b.dotProducts[2]);
 
 
-                let aCount = 0,
-                    bCount = 0;
-
-                if (a.dotProducts[0] > b.dotProducts[0]) aCount++; else bCount++;
-                if (a.dotProducts[1] > b.dotProducts[1]) aCount++; else bCount++;
-                if (a.dotProducts[2] > b.dotProducts[2]) aCount++; else bCount++;
-
-                let sum1 = a.dotProducts[0] + a.dotProducts[1] + a.dotProducts[2],
-                    sum2 = b.dotProducts[0] + b.dotProducts[1] + b.dotProducts[2];
-
-
-                //return sum1 - sum2;
-                return aCount - bCount;
+                    //return min1 - min2;
+                    //return sum1 - sum2;
+                    return aCount - bCount;
+                }
             });
 
             for (let i = 0; i < processedObjects.length; i++) {
-                const face = processedObjects[i].face;
+                if (processedObjects[i].constructor.name === "ProcessedPoint") {
+                    const point = processedObjects[i].point;
+                    
+                    const p = this.project(point.p);
+                    
+                    ctx.beginPath();
+                    ctx.arc(p.x, p.y, 5, 0, Math.PI * 2);
+                    ctx.fillStyle = point.material;
+                    ctx.fill();
+                } else if (processedObjects[i].constructor.name === "ProcessedFace") {
+                    
+                    const face = processedObjects[i].face;
 
-                const p1 = this.project(face.p1),
-                      p2 = this.project(face.p2),
-                      p3 = this.project(face.p3);
+                    const p1 = this.project(face.p1),
+                          p2 = this.project(face.p2),
+                          p3 = this.project(face.p3);
 
+                    
+
+                    /*
+                    const v1 = face.p2.makeCopy(),
+                          v2 = face.p3.makeCopy();
+
+                    v1.subtractPoint(face.p1);
+                    v2.subtractPoint(face.p1);
+
+                    const crossProduct = v1.crossProduct(v2);
+                    console.log(crossProduct)
+
+                    const dotProduct = crossProduct.dotProduct(lightSource) / crossProduct.getMagnitude();
+
+                    //console.log(dotProduct)
+
+                    */
+
+                    
+                    ctx.beginPath();
+                    ctx.moveTo(p1.x, p1.y);
+                    ctx.lineTo(p2.x, p2.y);
+                    ctx.lineTo(p3.x, p3.y);
+                    ctx.closePath();
+                    ctx.fillStyle = face.material;
+                    ctx.fill();
+                    
+                }
+                
+                
                 /*
-                const v1 = face.p2.makeCopy(),
-                      v2 = face.p3.makeCopy();
-
-                v1.subtractPoint(face.p1);
-                v2.subtractPoint(face.p1);
-
-                const crossProduct = v1.crossProduct(v2);
-                console.log(crossProduct)
-
-                const dotProduct = crossProduct.dotProduct(lightSource) / crossProduct.getMagnitude();
-
-                //console.log(dotProduct)
-
-                */
-
-                ctx.beginPath();
-                ctx.moveTo(p1.x, p1.y);
-                ctx.lineTo(p2.x, p2.y);
-                ctx.lineTo(p3.x, p3.y);
-                ctx.closePath();
-                /*ctx.fillStyle = face.material;
-                ctx.fill();*/
                 ctx.lineWidth = 2;
                 ctx.strokeStyle = face.material;
-                ctx.stroke();
+                ctx.stroke();*/
             }
             
+            /*
             for (let i = 0; i < this.objects.length; i++) {
                 const object = this.objects[i];
                 
@@ -93,7 +120,22 @@
                     ctx.arc(p.x, p.y, 5, 0, Math.PI * 2);
                     ctx.fillStyle = object.material;
                     ctx.fill();
+                } else if (object.constructor.name === "Line") {
+                    const p1 = this.project(object.p1),
+                          p2 = this.project(object.p2);
+                    
+                    ctx.beginPath();
+                    ctx.moveTo(p1.x, p1.y);
+                    ctx.lineTo(p2.x, p2.y)
+                    ctx.lineWidth = 2;
+                    ctx.strokeStyle = object.material;
+                    ctx.stroke();
                 }
+            }
+            */
+            
+            if (this.showAxes) {
+                // TODO
             }
         }
     }
@@ -147,6 +189,24 @@
             this.material = material;
         }
     }
+    
+    class ProcessedPoint {
+        constructor(point, cameraVector) {
+            //console.log(cameraVector)
+            
+            this.dotProduct = point.p.dotProduct(cameraVector);
+            this.point = point;
+            //console.log(this)
+        }
+    }
+    
+    class Line {
+        constructor(p1, p2, material) {
+            this.p1 = p1;
+            this.p2 = p2;
+            this.material = material;
+        }
+    }
 
     class Cuboid {
         constructor(p1, p2, sideMaterials) {
@@ -159,6 +219,8 @@
             const faces = [];
 
             for (let i = 0; i < 6; i++) {
+                //if (!(i === 1 || i === 4 || i === 3)) continue;
+                
                 let c1 = this.p1.makeCopy(),
                     c2 = this.p2.makeCopy(),
                     dimension = ["x", "y", "z"][Math.floor(i / 2)];
@@ -211,7 +273,8 @@
 
             // const biggestDotProduct = Math.max(Math.max(dotProductP1, dotProductP2), dotProductP3);
 
-            this.dotProducts = [dotProductP1, dotProductP2, dotProductP3];
+            this.dotProducts = [dotProductP1, dotProductP2, dotProductP3].sort(function(a, b) {return a-b});
+            //console.log(this.dotProducts)
             this.face = face;
         }
     }
@@ -221,6 +284,7 @@
         Scene: Scene,
         V: V,
         Cuboid: Cuboid,
-        Point: Point
+        Point: Point,
+        Line: Line
     };
 })();
